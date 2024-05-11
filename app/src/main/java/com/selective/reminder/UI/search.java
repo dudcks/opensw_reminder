a842e7763e3ac3c33a00020354d49dc61234edfa
dudcks <dudcks3886@naver.com>
2024-05-11 20:46:04 +0900

    Update search.java

--- app/src/main/java/com/selective/reminder/UI/search.java
+++ app/src/main/java/com/selective/reminder/UI/search.java
@@ -15,10 +15,6 @@ public class search extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        JsonObject a = new JsonObject();
-
-
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
 }
