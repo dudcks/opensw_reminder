674ec5c5ea64753d6c0d62451a0cada6e8c79777
ManaP0tion <45550827+Aer0dev@users.noreply.github.com>
2024-05-11 17:16:26 +0900

    login 부분 구현중

--- app/src/main/java/com/selective/reminder/UI/login.java
+++ app/src/main/java/com/selective/reminder/UI/login.java
@@ -1,8 +1,18 @@
 package com.selective.reminder.UI;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.selective.reminder.R;
+import com.google.gson.*;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +20,25 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+
+    protected void login(String id, String password) {
+        JSONObject jsonbody = new JSONObject();
+        // {
+        //    "username":"admin",
+        //    "password":"admin"
+        //}
+        try {
+            jsonbody.put("username", id);
+            jsonbody.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "json 오류", Toast.LENGTH_SHORT).show();
+        }
+
+        RequestQueue queue = Volley.newRequestQueue (getApplicationContext());
+
+
     }
 }
